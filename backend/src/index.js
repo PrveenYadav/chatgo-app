15464ea402e88e800be658220dfa9bc6213a6b01
PrveenYadav <bikyyadav13@gmail.com>
2026-06-22 08:25:31 +0530

    update package.json and index.js to include @clerk/express middleware and configure CORS

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,19 +3,23 @@ import cors from "cors";
 import "dotenv/config";
 // import dotenv from 'dotenv';
 // dotenv.config();
+import { clerkMiddleware } from "@clerk/express"; 
+import { connectDB } from "./lib/db.js";
 
 const app = express();
 
 app.use(express.json());
-app.use(cors());
+app.use(cors({origin: process.env.FRONTEND_URL, credentials: true}));
+app.use(clerkMiddleware());
 
 
-app.get('/', (req, res) => {
-    res.send("Backend is working");
+app.get('/health', (req, res) => {
+    res.status(200).json({ok: true});
 })
 
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
+    connectDB();
     console.log(`Your app is running on http://localhost:${port}`)
 })
