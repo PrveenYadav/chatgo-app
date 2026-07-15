368daaa3616775c192a77a95177164f564dd074c
PrveenYadav <bikyyadav13@gmail.com>
2026-07-16 04:10:22 +0530

    integrate Zustand for state management and add react-hot-toast for notifications

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,20 +6,42 @@ import { Navigate, Route, Routes } from 'react-router';
 import ChatPage from './pages/ChatPage.jsx';
 import AuthPage from './pages/AuthPage.jsx';
 import PageLoader from './components/PageLoader.jsx'
+import { useAuthStore } from './store/useAuthStore.js';
+import { useEffect } from 'react';
+import { Toaster } from 'react-hot-toast';
 
 function App() {
 
   const { isSignedIn, isLoaded } = useAuth();
 
-  if (!isLoaded) return <PageLoader />;
+  // option-1
+  // const { checkAuth, isCheckingAuth, clearAuth } = useAuthStore();
+
+  // option-2: better for performance
+  const clearAuth = useAuthStore((state) => state.clearAuth);
+  const checkAuth = useAuthStore((state) => state.checkAuth);
+  const isCheckingAuth = useAuthStore((state) => state.isCheckingAuth);
+
+  useEffect(() => {
+    if (!isLoaded) return;
+
+    if (isSignedIn) checkAuth();
+    else clearAuth();
+  }, [checkAuth, clearAuth, isLoaded, isSignedIn]);
+
+  if (!isLoaded || (isSignedIn && isCheckingAuth)) return <PageLoader />;
 
   return (
     <ThemeProvider>
       <WallpaperProvider>
+
         <Routes>
           <Route path="/" element={isSignedIn ? <ChatPage/> : <Navigate to={"/auth"} replace />}/>
           <Route path="/auth" element={!isSignedIn ? <AuthPage/> : <Navigate to={"/"} replace />}/>
         </Routes>
+
+        <Toaster/>
+
       </WallpaperProvider>
     </ThemeProvider>
   )
